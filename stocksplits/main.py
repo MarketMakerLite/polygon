7b28a85b62109e6121301748f5295ad427bc42a5
MML <83323207+MarketMakerLite@users.noreply.github.com>
2022-05-31 22:55:11 -0700

    Update main.py

--- stocksplits/main.py
+++ stocksplits/main.py
@@ -30,7 +30,7 @@ def sql_fun(df):
     postgreSQL_pool.putconn(conn)
 
 
-async def get_ticker_data(ticker, stocks_client):
+async def get_ticker_data(ticker):
     # Make API Call
     today = datetime.today().date()
     resp = await stocks_client.get_aggregate_bars(ticker, '2005-01-01', today, full_range=True, timespan='minute',
@@ -59,29 +59,30 @@ async def get_ticker_data(ticker, stocks_client):
     return df
 
 
-async def main(symbol_list, stocks_client):
+async def main(symbol_list):
     print(f"Getting data for {len(symbol_list)} symbols")
     for ticker in symbol_list:
         print(f"Getting data for {ticker}")
         try:
-            df = await get_ticker_data(ticker, stocks_client)
-            # Save to database
+            df = await get_ticker_data(ticker)
+            Save to database
             clear_data = text(f"""DELETE FROM stockdata_hist WHERE symbol = '{ticker}';""")
             with engine.connect() as conn:
-                conn.execute(clear_data)
+                 conn.execute(clear_data)
             sql_fun(df)
+            print(df)
         except Exception as e:
             print(e)
             pass
 
 
-async def stock_splits(symbol_list):
+def stock_splits(symbol_list):
     splits_list = []
     execution_date = datetime.today().date()
     print(execution_date)
     for ticker in symbol_list:
         print(f"Checking for splits: {ticker}")
-        resp = await async_reference_client.get_stock_splits(ticker, all_pages=True, execution_date=execution_date)
+        resp = reference_client.get_stock_splits(ticker, all_pages=True, execution_date=execution_date)
         if resp:
             if resp[0]['ticker']:
                 splits_list.append(resp[0]['ticker'])
@@ -94,16 +95,16 @@ if __name__ == '__main__':
     engine = create_engine(config.psql)
     postgreSQL_pool = psycopg2.pool.ThreadedConnectionPool(5, 50, host=config.psql_host, database=config.psql_db,
                                                            user=config.psql_user, password=config.psql_pw)
-    async_reference_client = polygon.ReferenceClient(config.polygon_key, True)
+    reference_client = polygon.ReferenceClient(config.polygon_key, False, read_timeout=60)
     symbols_df = pd.read_sql_query('select ticker from companies where active = true', con=engine)
     symbols = symbols_df['ticker'].to_list()
+    # symbols = ['FAMI', 'YINN', 'ERY', 'CWEB', 'BQ']  # Example List
     stock_cols = ['symbol', 'tick_volume', 'total_volume', 'opening_price', 'tick_vwap', 'tick_open', 'tick_close',
                   'tick_high', 'tick_low', 'vwap', 'avg_trade_size', 'time_beg', 'time_end', 'tdate', 'save_date']
-
-    splits_list = asyncio.run(stock_splits(symbols))
+    
+    splits_list = stock_splits(symbols)
     print(splits_list)
-    async_reference_client.close()
-
+    reference_client.close()
     stocks_client = StocksClient(config.polygon_key, True)
     asyncio.run(main(splits_list, stocks_client))
     stocks_client.close()
