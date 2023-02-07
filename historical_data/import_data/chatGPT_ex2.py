bc28cb954665d521277361857606cb1e46655baa
Eric Maynard <eric@eruditis.com>
2023-02-07 13:50:14 -0600

    playing around with GPT autocoding

---
+++ historical_data/import_data/chatGPT_ex2.py
@@ -0,0 +1,101 @@
+import aiohttp
+import asyncio
+import datetime
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import time
+from typing import Any, Dict, List, Tuple
+import json
+
+
+# Part 1: Asynchronous download of data
+
+async def download_trade_data(symbol: str, date: str, session: aiohttp.ClientSession) -> Dict[str, Any]:
+    # Replace YOUR_API_KEY with your actual API key
+    API_KEY = "ipWaWpJ9fdx_tsglcFa4nYjYBsaLc9op"
+
+    # Set the headers for the request
+    headers = {
+        "Authorization": f"Bearer {API_KEY}"
+    }
+
+    """Asynchronously download trade data for the given date from Polygon.io"""
+    base_url = 'https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/{multiplier}/{timespan}/{from}/{to}'
+    params = {
+        'stocksTicker' : symbol,
+        'multiplier' : '1', # Interval in minutes
+        'timespan' : 'minute',
+        'from' : date,
+        'to' : date
+    }
+    url = base_url.format(**params)
+
+    async with session.get(url, headers=headers) as resp:
+        data = await resp.read()
+        trade_data = json.loads(data)
+    return trade_data
+
+
+async def download_trade_data_for_symbol(symbol: str, date: str) -> Dict[str, Any]:
+    """Download trade data for a given symbol and date using an asyncio event loop"""
+    async with aiohttp.ClientSession() as session:
+        data = await download_trade_data(symbol, date, session)
+    return data
+
+
+async def download_trade_data_for_symbols(symbols: List[str], date: str) -> List[Dict[str, Any]]:
+    """Download trade data for a list of symbols and a given date using asyncio tasks"""
+    tasks = [asyncio.create_task(download_trade_data_for_symbol(symbol, date)) for symbol in symbols]
+    data = await asyncio.gather(*tasks)
+    return data
+
+
+# Part 2: Writing to Parquet files
+
+def write_data_to_parquet(data: List[Dict[str, Any]], date: str) -> None:
+    for dict in data:
+        """Write data to a Parquet file with a filename based on the date"""
+        # Convert data to Arrow Table
+        # table = pa.Table.from_pydict({k: pa.array(v) for k, v in data.items()})
+        df = pd.DataFrame.from_dict(dict)
+
+        # Convert the DataFrame to a Parquet file
+        table = pa.Table.from_pandas(df)
+
+        # Set filename based on date
+        filename = f"trade_data_{date}.parquet"
+
+        # Write Arrow Table to Parquet file
+        pq.write_table(table, filename)
+
+
+# Part 3: Main function to run everything
+
+def main(symbols: List[str], start_date: str, end_date: str) -> None:
+    """Main function to download and write trade data for a list of symbols and a given date range"""
+    # Convert start and end dates to date objects
+    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+
+    # Loop through each day in the date range
+    for i in range((end_date - start_date).days + 1):
+        date = start_date + datetime.timedelta(days=i)
+
+        # Download trade data
+        data = asyncio.run(download_trade_data_for_symbols(symbols, date))
+
+        # Write data to Parquet file
+        write_data_to_parquet(data, date)
+
+
+if __name__ == "__main__":
+    # List of symbols to download trade data for
+    symbols = ["AAPL", "GOOG", "MSFT"]
+
+    # Start and end dates for date range
+    start_date = "2022-01-04"
+    end_date = "2022-01-07"
+
+    main(symbols, start_date, end_date)
+
---
+++ historical_data/import_data/chatGPT_example.py
@@ -0,0 +1,70 @@
+import asyncio
+import aiohttp
+import concurrent.futures
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+
+async def download_data(session, url):
+    """Asynchronously download data from the given URL"""
+    async with session.get(url) as response:
+        return await response.read()
+
+
+async def download_data_for_day(date, api_key):
+    """Asynchronously download trade data for the given date from Polygon.io"""
+    base_url = 'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{date}/{date}/{interval}'
+    """use a dictionary to store the arguments to the format function"""
+    params = {
+        'api_key': api_key,
+        'interval': '1'  # Interval in minutes
+    }
+    async with aiohttp.ClientSession() as session:
+        # Create tasks to download data for each ticker in parallel
+        tasks = []
+        for ticker in tickers:
+            url = base_url.format(ticker=ticker, date=date, **params)
+            task = asyncio.ensure_future(download_data(session, url))
+            tasks.append(task)
+
+        # Wait for all tasks to complete and retrieve results
+        data = await asyncio.gather(*tasks)
+
+        # Process and return data as needed
+        return process_data(data)
+
+
+def write_data_to_parquet(data, date):
+    """Write the given data to a Parquet file for the given date"""
+    # Convert data to a PyArrow Table
+    table = pa.Table.from_pandas(data)
+
+    # Write the table to a Parquet file
+    pq.write_table(table, '{}.parquet'.format(date))
+
+
+def download_and_write_data(dates, api_key):
+    """Download and write trade data for the given dates using parallel asynchronous threads"""
+    loop = asyncio.get_event_loop()
+
+    # Create a thread pool with a fixed number of threads
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        # Create tasks to download data for each date in parallel
+        tasks = []
+        for date in dates:
+            task = loop.run_in_executor(executor, download_data_for_day, date, api_key)
+            tasks.append(task)
+
+        # Wait for all tasks to complete and retrieve results
+        data_by_day = loop.run_until_complete(asyncio.gather(*tasks))
+
+        # Write data to Parquet files
+        for date, data in zip(dates, data_by_day):
+            write_data_to_parquet(data, date)
+
+
+# Define the dates for which to download data
+dates = ['2022-01-01', '2022-01-02', '2022-01-03']
+
+# Download and write data using parallel asynchronous threads
+download_and_write_data(dates, 'ipWaWpJ9fdx_tsglcFa4nYjYBsaLc9op')
