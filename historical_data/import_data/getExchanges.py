43a2050ee2ea906ed02442bcecec737ce2ea9b57
Eric Maynard <eric@eruditis.com>
2023-02-07 13:50:00 -0600

    example of getting exchanges

---
+++ historical_data/import_data/getExchanges.py
@@ -0,0 +1,28 @@
+import asyncio
+import aiohttp
+
+async def get_exchanges():
+    # Replace YOUR_API_KEY with your actual API key
+    API_KEY = "ipWaWpJ9fdx_tsglcFa4nYjYBsaLc9op"
+
+    # Set the base URL for the Polygon API
+    BASE_URL = "https://api.polygon.io"
+
+    # Set the headers for the request
+    headers = {
+        "Authorization": f"Bearer {API_KEY}"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{BASE_URL}/v3/reference/exchanges", headers=headers) as response:
+            if response.status == 200:
+                exchanges = await response.json()
+                for exchange in exchanges["results"]:
+                    print(exchange)
+            else:
+                print(f"Request failed with status code {response.status}")
+                print(response.reason)
+
+# Run the function asynchronously
+asyncio.run(get_exchanges())
+
